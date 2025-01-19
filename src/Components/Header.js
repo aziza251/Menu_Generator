bcf7da1dbf52e6be4bad2a85fd17f62053becb9a
aziza <urinboevaaziza3@gmail.com>
2025-01-19 16:02:07 +0300

    logo added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,14 @@
 import React from "react";
 import "./Header.css";
 import About from "../Pages/About";
+import LogoImage from "./Assets/logoo.webp";
 
 const Header = () => {
   return (
     <nav className="header">
       <div className="header__icons">
         <a href="/">
-          <img
-            className="header__logo"
-            src="https://files.oaiusercontent.com/file-QmPHngdCqtJZ3Xj4Sc6gJM?se=2025-01-18T17%3A50%3A15Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D7f33b5dc-5da1-4248-b921-ec3473f49010.webp&sig=6qV37eoD2WAlNg/UBrB/Sm1dU%2B2iPOvnfBU6%2BBAngv0%3D"
-            alt="header_logo"
-          />
+          <img className="header__logo" src={LogoImage} alt="header_logo" />
         </a>
 
         <a href="/about">
